9ab23eaf80fccbcf212a4a0963712e5c2d1aacd6
Thai Nguyen <thainguyen20594@gmail.com>
2023-09-12 17:24:23 -0400

    Add files via upload

---
+++ LinearSearch.java
@@ -0,0 +1,16 @@
+package ArrayTest;
+
+public class LinearSearch {
+    public static int linearSearch(int[] list, int key) {
+        for (int i = 0; i < list.length; i++) {
+            if (key == list[i])
+                return i;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] list = {4, 5, 1, 2, 9, -3};
+        System.out.println(linearSearch(list, 99));
+    }
+}
