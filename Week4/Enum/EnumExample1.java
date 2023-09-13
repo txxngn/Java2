48c3c4fa20388445167881cc127a93a65c085bfd
Thai Nguyen <thainguyen20594@gmail.com>
2023-09-13 11:22:49 -0400

    Add files via upload

---
+++ Week4/Enum/EnumExample1.java
@@ -0,0 +1,15 @@
+package Enum;
+public class EnumExample1 {
+      enum Size {
+        SMALL,
+        LARGE,
+        MEDIUM
+    }
+
+    public static void main(String[] args) {
+        Size s1 = Size.SMALL;
+        System.out.println(s1);
+    }
+}
+
+
