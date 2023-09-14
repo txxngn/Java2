1696cd228ce57a22e978cd63af8930c9bd509788
Thai Nguyen <thainguyen20594@gmail.com>
2023-09-14 10:44:57 -0400

    Add files via upload

---
+++ Week4/Enum/Test_EnumExample1_v2.java
@@ -0,0 +1,15 @@
+ 
+package Enum;
+public class Test_EnumExample1_v2 {
+
+    public static void main(String[] args) {
+       //e s2= e.MEDIUM;
+       //System.out.println(s2);
+        
+        Size s1=Size.MEDIUM;// this will work even if Size.java is not public
+        System.out.println(s1);
+
+        
+
+}
+}
