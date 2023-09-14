c20c815a7b27f07040e6a5b91e74b3ba1d3e6020
Thai Nguyen <thainguyen20594@gmail.com>
2023-09-14 10:44:20 -0400

    Add files via upload

---
+++ Week4/Enum/First.java
@@ -0,0 +1,11 @@
+package Enum;
+
+public class First {
+
+    public static void main(String[] args) {
+        final int x = 100;
+        //x = x - 10;           //dòng này cmt vì k thể thay đổi x, uncmt thể xem báo lỗi
+        System.out.println(x);
+    }
+
+}
