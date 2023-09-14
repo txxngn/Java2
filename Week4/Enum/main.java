12b27d2844c8c1b2dd161b69bd9f27adeed76a26
Thai Nguyen <thainguyen20594@gmail.com>
2023-09-14 10:44:40 -0400

    Add files via upload

---
+++ Week4/Enum/main.java
@@ -0,0 +1,15 @@
+ 
+package Enum;
+
+public class main {
+
+    public static void main(String[] args) {
+        Size s2= Size.MEDIUM;
+        System.out.println(s2);
+        //Circle c1=new Circle();
+       // week3.Circle c1=new week3.Circle();
+
+        //System.out.println(c1);
+    }
+
+}
