9935d0679046bd1ac34cadfb64c1ad452d54162a
Thai Nguyen <thainguyen20594@gmail.com>
2023-09-14 10:44:50 -0400

    Add files via upload

---
+++ Week4/Enum/Size.java
@@ -0,0 +1,7 @@
+package Enum;
+
+  public enum Size {//public or not public we can use this enum within the same package
+    SMALL,   // if not public you not use outside the package
+    LARGE,
+    MEDIUM
+}
