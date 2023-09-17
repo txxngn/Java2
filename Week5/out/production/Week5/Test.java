382c81702b0f441052f8267e8694ba2284f829e7
Thai Nguyen <thainguyen20594@gmail.com>
2023-09-17 09:09:09 -0400

    Add files via upload

---
+++ Week5/out/production/Week5/Test.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Test {
+    public static void main (String [] args){
+        ArrayList<String> name = new ArrayList();
+        name.add("abc");
+        name.add("bbbb");
+        name.get(1);
+        System.out.println(name.get(1));
+    }
+}
