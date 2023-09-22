35130888fb886eb18b1220c903cc431feac36799
Thai Nguyen <thainguyen20594@gmail.com>
2023-09-22 09:09:39 -0400

    Add files via upload

---
+++ Week7/Account.java
@@ -0,0 +1,17 @@
+package ObjectBinaryIO;
+
+import java.io.*;
+public class Account implements Serializable {
+    String name;
+    String id;
+    double balance;
+    public Account (String name, String id, double balance){
+        this.name=name;
+        this.id = id;
+        this.balance = balance;
+    }
+
+    public String toString(){
+        return "ObjectBinaryIO.Account [Name: " +name + ", ID" + id + ", Balance: " + balance;
+    }
+}
